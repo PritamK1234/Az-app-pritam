ca9f1ccd5b564cc282678b5fb59f36a229c9390c
Pritam S Kadam <pritamkadam1234@gmail.com>
2026-06-22 14:52:07 +0530

    Add project files.

---
+++ Az-app-pritam/Pages/Privacy.cshtml.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Az_app_pritam.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+
+}
