ebabe0322697d803f6864f5a520b497b4d641df9
Pritam S Kadam <pritamkadam1234@gmail.com>
2026-06-23 10:25:25 +0530

    Trigerring CI CD Pipeline to see added appsetting.json file config are added or not in Azure App Service Editor

--- Az-app-pritam/Pages/Index.cshtml.cs
+++ Az-app-pritam/Pages/Index.cshtml.cs
@@ -5,8 +5,16 @@ namespace Az_app_pritam.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly IConfiguration _config;
+        public IndexModel(ILogger<IndexModel> logger, IConfiguration config)
+        {
+            _config = config;
+            _logger = logger;
+        }
         public void OnGet()
         {
+            ViewData["Greetings"] = _config["Greetings"];
 
         }
     }
